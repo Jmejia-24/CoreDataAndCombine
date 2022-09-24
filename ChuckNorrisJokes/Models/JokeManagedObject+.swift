30186aaa14d306cdc5ae59817e64470276376805
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-09-24 08:46:26 -0600

    Feat: added JokeManagedObject to save jokes

---
+++ ChuckNorrisJokes/Models/JokeManagedObject+.swift
@@ -0,0 +1,36 @@
+//
+//  JokeManagedObject+.swift
+//  ChuckNorrisJokes
+//
+//  Created by User on 9/24/22.
+//  Copyright © 2022 Byron Mejia. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+import ChuckNorrisJokesModel
+
+extension JokeManagedObject {
+    static func save(joke: Joke, inViewContext viewContext: NSManagedObjectContext) {
+        guard joke.id != "error" else { return }
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: JokeManagedObject.self))
+        fetchRequest.predicate = NSPredicate(format: "id = %@", joke.id)
+        
+        if let results = try? viewContext.fetch(fetchRequest),
+           let existing = results.first as? JokeManagedObject {
+            existing.value = joke.value
+            existing.categories = joke.categories as NSArray
+        } else {
+            let newJoke = self.init(context: viewContext)
+            newJoke.id = joke.id
+            newJoke.value = joke.value
+            newJoke.categories = joke.categories as NSArray
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            fatalError("\(#file), \(#function), \(error.localizedDescription)")
+        }
+    }
+}
