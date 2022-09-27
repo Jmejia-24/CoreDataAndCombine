8bcf5abb17f7cba4f18a07306e0eb01b6911c2dc
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-09-27 07:26:39 -0600

    Feat: added core data stack

--- ChuckNorrisJokes/App/SceneDelegate.swift
+++ ChuckNorrisJokes/App/SceneDelegate.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import SwiftUI
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         let contentView = JokeView()
+          .environment(\.managedObjectContext, CoreDataStack.viewContext)
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
@@ -24,10 +26,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-        
-        // Save changes in the application's managed object context when the application transitions to the background.
+        CoreDataStack.save()
+    }
+}
+
+private enum CoreDataStack {
+    static var viewContext: NSManagedObjectContext = {
+        let container = NSPersistentContainer(name: "ChuckNorrisJokes")
+        container.loadPersistentStores { _, error in
+            guard error == nil else {
+                fatalError("\(#file), \(#function), \(error!.localizedDescription)")
+            }
+        }
+        return container.viewContext
+    }()
+    
+    static func save() {
+        guard viewContext.hasChanges else { return }
+        do {
+            try viewContext.save()
+        } catch {
+            fatalError("\(#file), \(#function), \(error.localizedDescription)")
+        }
     }
 }
